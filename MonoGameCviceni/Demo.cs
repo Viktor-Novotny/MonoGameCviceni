d724acf173ef3d4342160cf023e6394b0d712877
Novotný Viktor <novotnyv@spstrutnov.cz>
2020-11-29 17:11:09 +0100

    Základní vlastnosti

--- MonoGameCviceni/Demo.cs
+++ MonoGameCviceni/Demo.cs
@@ -9,16 +9,28 @@ namespace MonoGameCviceni
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private int _sirkaOkna = 800;
+        private int _vyskaOkna = 600;
+
+        private Texture2D _textura;
+
+        private int h, w, x, y, prumer, polomer;
+
+        private Color barva;
+
         public Demo()
         {
             _graphics = new GraphicsDeviceManager(this);
+            Window.Title = "MonoGame";
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = _sirkaOkna;
+            _graphics.PreferredBackBufferHeight = _vyskaOkna;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -27,7 +39,21 @@ namespace MonoGameCviceni
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            prumer = 100;
+            polomer = prumer / 2;
+            barva = Color.White;
+
+            w = 100;
+            h = 100;
+            x = _sirkaOkna / 2 - w / 2;
+            y = _vyskaOkna / 2 - h / 2;
+
+            Color[] pixely = new Color[prumer * prumer];
+
+            _textura = new Texture2D(GraphicsDevice, prumer, prumer);
+            _textura.SetData(pixely);
+
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,7 +70,9 @@ namespace MonoGameCviceni
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_textura, new Rectangle(x, y, w, h), barva);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
